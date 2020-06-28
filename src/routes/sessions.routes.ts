186cc3fb3b8033b44667d2c4315c30923c0bd0a6
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-06-28 19:59:21 -0300

    chore:improvement

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,22 +5,18 @@ import CreateSessionServices from '../services/sessions/CreateServices';
 const sessionsRoute = Router();
 
 sessionsRoute.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const createSessionServices = new CreateSessionServices();
+  const createSessionServices = new CreateSessionServices();
 
-    const { user, token } = await createSessionServices.execute({
-      email,
-      password,
-    });
+  const { user, token } = await createSessionServices.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (Err) {
-    return response.status(500).json({ Err });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRoute;
